893efc405c14fb139157f48908b02f31a18f4ed2
ry.yamafuji@gmail.com <ry.yamafuji@gmail.com>
2023-02-02 15:47:11 +0900

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MTg5MQ/spectral.js?branch_slug=master&token=0dc8c208-2be2-452f-a8fc-96674510923c";
+export default { extends: ruleset };
